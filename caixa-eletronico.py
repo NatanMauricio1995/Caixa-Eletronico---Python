8ac974aa4f898284932a90fb6d6db5004a94411e
Natan Mauricio Santos <natanmauriciosantos@exemplo.com>
2025-07-24 16:08:10 -0300

    Seleção de escolhas - concluido

--- caixa-eletronico.py
+++ caixa-eletronico.py
@@ -14,24 +14,58 @@ Funcionalidades:
 ✅ Sistema de senhas
 ✅ Relatórios detalhados
 """
-
+"""
+Pendências
+ - Exibir o nome do usuário logado no menu principal    
+    """
 def menu():
     #Exibir o menu principal com as escolhas de opções
     print("-"* 40)
     print(f"{" "*13}MENU PRINCIPAL")
     print("-" * 40)
-    
-    #Imprimir o nome do Usuário
 
     print("1️⃣  - Consultar Saldo")
-    print("2️⃣  - Depositar Dinheiro")
-    print("3️⃣  - Sacar Dinheiro")
+    print("2️⃣  - Depósito")
+    print("3️⃣  - Saque")
     print("4️⃣  - Histórico de Transações")
     print("5️⃣  - Relatório Detalhado")
     print("6️⃣  - Sair")
     print("-" * 40)
+    
+def escolha():
+    while (opcao != 6):
+        menu()
+        try:
+            opcao = int(input("Digite a opção desejada:"))
+            '''
+            if (opcao == 1):
+                #Consultar saldo
+            elif (opcao == 2):
+                #Depósito
+            elif (opcao == 3):
+                #Saque
+            elif (opcao == 4):
+                #Histório de transições
+            elif (opcao == 5):
+                #Relatório Detalhado
+            elif (opcao == 6):
+                #Sair
+            else:
+                print("❌ Opção inválida! Digite um número de 1 a 6.")
+                input("\n Pressione ENTER para continuar...")
+            '''
+        except KeyboardInterrupt:
+            print("\n\n🛑 Sistema interrompido pelo usuário.")
+            break
+        except Exception as e:
+            print(f"❌ Erro inesperado: {e}")
+            input("\n📱 Pressione ENTER para continuar...")
+    
+    
+
         
+
     
 #Programa principal
 
-menu()
+escolha()
